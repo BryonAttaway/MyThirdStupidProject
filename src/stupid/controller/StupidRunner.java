8f73543fcf58497c2957f6c45c783d85089f1783
BryonAttaway <DrakonUp@gmail.com>
2014-09-16 08:22:16 -0600

    Add to repo

---
+++ src/stupid/controller/StupidRunner.java
@@ -0,0 +1,13 @@
+package stupid.controller;
+
+public class StupidRunner 
+{
+
+	public static void main(String[] args) 
+	{
+		ThirdStupid myStupid = new ThirdStupid();
+		myStupid.start();
+		
+	}
+
+}
---
+++ src/stupid/controller/ThirdStupid.java
@@ -0,0 +1,21 @@
+package stupid.controller;
+
+public class ThirdStupid 
+{
+
+	public void start()
+	{
+		myIntroduction();
+		myIntroduction();
+		myIntroduction();
+		myIntroduction();
+	}
+	
+	private void myIntroduction()
+	{
+		System.out.println("Im Bryon");
+		System.out.println("I love money!!!");
+		System.out.println("I love camping!!!");
+		System.out.println("I hate onions.");
+	}
+}
